7e1c7f0de7670cc82b5bb4da34e50947a75b0853
The-vinicuis <viniciusnao41@gmail.com>
2024-01-10 21:19:09 -0300

    web state load url

---
+++ lib/ui/state/web_state.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebState with ChangeNotifier {
+  static final controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    );
+
+  void loadPage(url) {
+    controller.loadRequest(Uri.parse(url));
+    notifyListeners();
+  }
+}
