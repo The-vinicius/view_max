c16bdeb0807ca28ca8b0343d9fc4ba2814979b45
The-vinicuis <viniciusnao41@gmail.com>
2024-01-10 21:21:25 -0300

    converter stateful

--- lib/ui/screen/screen_home.dart
+++ lib/ui/screen/screen_home.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:view_max/ui/widget/search_widget.dart';
+import 'package:view_max/ui/widget/web_view_show.dart';
 
-class ScreenHome extends StatelessWidget {
+class ScreenHome extends StatefulWidget {
   const ScreenHome({super.key});
 
   @override
+  State<ScreenHome> createState() => _ScreenHomeState();
+}
+
+class _ScreenHomeState extends State<ScreenHome> {
+  final TextEditingController controller = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        appBar: SearchWidget(),
-        body: Center(child: Text('centralizado')),
+        appBar: SearchWidget(
+          controller: controller,
+        ),
+        body: const WebViewShow(),
       ),
     );
   }
