3b004b5e09770b57caaa6f5fce04354e835a4bc3
The-vinicuis <viniciusnao41@gmail.com>
2024-01-10 21:20:32 -0300

    listenable web state

--- lib/ui/widget/search_widget.dart
+++ lib/ui/widget/search_widget.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:view_max/ui/state/web_state.dart';
 
 class SearchWidget extends StatelessWidget implements PreferredSizeWidget {
-  const SearchWidget({super.key});
+  const SearchWidget({super.key, required this.controller});
+  final TextEditingController controller;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: const SearchBar(),
-    );
+    final webState = WebState();
+    return ListenableBuilder(
+        listenable: webState,
+        builder: (context, widget) {
+          return Container(
+            child: TextField(
+              controller: controller,
+              decoration: const InputDecoration(hintText: 'pesquisar'),
+              onSubmitted: (value) {
+                webState.loadPage(controller.text);
+              },
+            ),
+          );
+        });
   }
 
   @override
